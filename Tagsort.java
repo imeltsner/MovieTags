626a34ed6af55a1b09adb54c84e0d5d4e72f845d
isaac meltsner <itmeltsner@dons.usfca.edu>
2023-02-15 19:36:40 -0800

    fixed sort, added print popularity

--- Tagsort.java
+++ Tagsort.java
@@ -3,62 +3,88 @@ import java.util.Scanner;
 import java.io.IOException;
 import java.io.File;
 
-//Reads a list of movie tags from a CSV and sorts them lexicographically
+//Reads a list of movie tags from a CSV
 public class Tagsort {
-    ArrayList<String> tags = new ArrayList<String>();
+    ArrayList<String> seenTags = new ArrayList<String>();
+    ArrayList<Category> categories = new ArrayList<Category>();
+    ArrayList<Category> sortedCategories = new ArrayList<Category>();
 
     Tagsort(String filePath) {
         try {
             boolean firstLine = true;
             Scanner scan = new Scanner(new File(filePath));
-            int c = 0;
             while (scan.hasNextLine()) {
-                String tag = scan.nextLine().split(",")[2];
-                tags.add(tag);
+                if (!firstLine) {
+                    String tag = scan.nextLine().split(",")[2];
+                    if (!seenTags.contains(tag)) {
+                        seenTags.add(tag);
+                        Category movie = new Category(tag);
+                        categories.add(movie);
+                        sortedCategories.add(movie);
+                    } else {
+                        categories.get(seenTags.indexOf(tag)).addOne();
+                        sortedCategories.get(seenTags.indexOf(tag)).addOne();
+                    }
+                }
                 if (firstLine) {
-                    tags.clear();
                     firstLine = !firstLine;
-                }
-                
-                c++;
-                if (c > 1000) {
-                    break;
-                }
+                } 
             }
             scan.close();
         } catch (IOException e) {
             System.err.println("FILE NOT FOUND");
         }
-    //    System.out.println("File Read Success: tags[0] is " + tags.get(0));
     }
 
     public void swap(int i, int j) {
-        String temp = tags.get(j);
-        tags.set(j, tags.get(i));
-        tags.set(i, temp);
+        Category temp = sortedCategories.get(j);
+        sortedCategories.set(j, sortedCategories.get(i));
+        sortedCategories.set(i, temp);
     }
 
     public int findSmallest(int start) {
-        //returns index of smallest value in array
+        //returns index of Largest value in array
         int smallestIndex = start;
-        for (int i = start+1; i < tags.size(); i++) {
-            if (tags.get(i).compareTo(tags.get(smallestIndex)) <= 0) {
+        for (int i = start+1; i < sortedCategories.size(); i++) {
+            if (sortedCategories.get(i).getCount() < sortedCategories.get(smallestIndex).getCount()) {
                 smallestIndex = i;
+            }
+            if (sortedCategories.get(i).getCount() == sortedCategories.get(smallestIndex).getCount()) {
+                if (sortedCategories.get(i).getTag().compareTo(sortedCategories.get(smallestIndex).getTag()) <= 0) {
+                    smallestIndex = i;
+                }
             }
         }
         return smallestIndex;
     }
 
     public void sort() {
-        for (int i = 0; i < tags.size(); i ++) {
-            swap(i, findSmallest(i)); //swaps the smallest value with the value at i
+        for (int i = 0; i < sortedCategories.size(); i ++) {
+            swap(i, findSmallest(i)); //swaps the largest value with the value at i
         }
-        for (int i = 0; i < tags.size(); i++) {
-            System.out.println(tags.get(i));
-        }
+        /*
+        for (int i = 0; i < 50; i++) {
+            sortedCategories.get(i).print();
+        } */
     }
 
-    public ArrayList<String> getList() {
-        return tags;
+    public void printPopularity() {
+        System.out.println("*** Highest 3 movies by count ***");
+        sortedCategories.get(sortedCategories.size()-1).print();
+        sortedCategories.get(sortedCategories.size()-2).print();
+        sortedCategories.get(sortedCategories.size()-3).print();
+        System.out.println("*** Lowest 3 movies by count");
+        sortedCategories.get(0).print();
+        sortedCategories.get(1).print();
+        sortedCategories.get(2).print();
+        
+    }
+
+    public ArrayList<Category> getCategories() {
+        return categories;
+    }
+
+    public ArrayList<String> getTagList() {
+        return seenTags;
     }
 }
